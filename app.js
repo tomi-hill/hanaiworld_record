ff6eb9348029090835b1d33a508e729c9dd2a1c1
tomi-hill <tommy.eagles@icloud.com>
2026-09-22 02:28:17 +0900

    Update app.js

--- app.js
+++ app.js
@@ -142,9 +142,11 @@ try{
     const lo = validSorted[n/2 - 1];
     const hi = validSorted[n/2];
     medianCard = {
-      cls:'med', kicker:'中央値 MEDIAN', val:fmtScore(s.median_val)+'点',
-      name: lo && hi ? `${lo.name.split(/\r?\n/)[0].trim()} <br> ${hi.name.split(/\r?\n/)[0].trim()}` : '',
-      desc: lo && hi ? `${lo.name.replace(/\n/g,' ')} <br> ${hi.name.replace(/\n/g,' ')}` : ''
+     cls:'med',
+     kicker:'中央値 MEDIAN',
+     val:fmtScore(s.median_val)+'点',
+     name: lo && hi ? `${lo.name.split(/\r?\n/)[0].trim()} <br> ${hi.name.split(/\r?\n/)[0].trim()}` : '',
+     desc: lo && hi ? `${lo.name.split(/\r?\n/)[0].trim()} <br> ${hi.name.split(/\r?\n/)[0].trim()}` : ''
     };
   }
   const cards = [
