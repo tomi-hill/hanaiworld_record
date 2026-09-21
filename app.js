9a72b80b3acd2d81ced50078dc391e3659415bbe
tomi-hill <tommy.eagles@icloud.com>
2026-09-22 02:02:50 +0900

    Update app.js

--- app.js
+++ app.js
@@ -143,7 +143,7 @@ try{
     const hi = validSorted[n/2];
     medianCard = {
       cls:'med', kicker:'中央値 MEDIAN', val:fmtScore(s.median_val)+'点',
-      name: lo && hi ? `${lo.name.replace(/\n/g,' ')}（${fmtScore(lo.score_num)}点）<br> ${hi.name.replace(/\n/g,' ')}（${fmtScore(hi.score_num)}点）` : '',
+      name: lo && hi ? `${lo.name.replace(/\n/g,' ')} <br> ${hi.name.replace(/\n/g,' ')}` : '',
       desc: lo && hi ? `${lo.name.replace(/\n/g,' ')}（${fmtScore(lo.score_num)}点）<br> ${hi.name.replace(/\n/g,' ')}（${fmtScore(hi.score_num)}点）の平均` : ''
     };
   }
